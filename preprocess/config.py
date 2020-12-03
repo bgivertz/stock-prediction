dc9e09158b13ff6cdb8078c8b9a425cfacf865d9
georgiagallant333 <georgia.gallant@gmail.com>
2020-12-02 20:25:56 -0500

    new tweet data

--- preprocess/config.py
+++ preprocess/config.py
@@ -1,10 +1,9 @@
 # TWITTER PREPROCESS QUERY PARAMETERS
 # format: year-month-day (0000-00-00)
-date_range_start = '2008-01-01'
+date_range_start = '2015-01-01'
 date_range_end = '2020-11-01'
 max_num_tweets = 100
 #removed avgo because doesn't have data before 2009
-
 stock_listing_symbols = ['AAPL', 'ACN', 'ADBE', 'ADBE', 'AMD', 'CAJ', 'CSCO', 'GOOGL', 'GRMN', 'IBM', 'INTC', 'MSFT', 'MSI', 'NVDA', 'QQQ', 'TXN']
 n_days = '10'  # specifies which stock directory we are using so n_days = 5 means looking in data_5
 # hashtags to look for in tweets
